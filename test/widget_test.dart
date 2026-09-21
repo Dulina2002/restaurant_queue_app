f356a9f12c5551e10ff868e51bcf238951c6b5e7
lasitharathnayaka <83506365+lasitharathnayaka@users.noreply.github.com>
2026-09-21 17:00:17 +0530

    fix: resolve Dart 3.7 SDK constraint and add material test import

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:restaurant_queue_app/models/user_role.dart';
 import 'package:restaurant_queue_app/models/user_profile.dart';
